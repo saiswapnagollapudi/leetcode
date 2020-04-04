5ba9d75edb2bcc977fb0b1d3e0f20e786fd27e1a
swapsha <saiswapnagollapudi@gmail.com>
2020-04-03 23:31:04 -0400

    Update max_subarray.py

--- leet_code/Maximum Subarray/max_subarray.py
+++ leet_code/Maximum Subarray/max_subarray.py
@@ -8,6 +8,6 @@
         sum_n = 0
         for i in nums:
             sum_n += i
-            max_n = max(sum_n, max_n, i)
+            max_n = max(sum_n, max_n)
             sum_n = max(sum_n,0)
         return(max_n)
