8377b89880195eb1aa107aa0883d8ca4a0821725
swapsha <saiswapnagollapudi@gmail.com>
2020-04-03 23:26:10 -0400

    Create max_subarray.py

---
+++ leet_code/Maximum Subarray/max_subarray.py
@@ -0,0 +1,13 @@
+ class Solution(object):
+     def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_n = nums[0]
+        sum_n = 0
+        for i in nums:
+            sum_n += i
+            max_n = max(sum_n, max_n, i)
+            sum_n = max(sum_n,0)
+        return(max_n)
